41965e4ed7df0bdf16ac27cf33750bf7941e853d
ParikshidArya <parikshid2005@gmail.com>
2025-11-10 21:47:27 +0530

    Stage 2: Try and except added

--- Calculator.py
+++ Calculator.py
@@ -1,22 +1,37 @@
 num1 = input("Enter first number:")
 num2 = input("Enter second number:")
-operation = input("Enter operator ( +, - , / , * ) :")
 
-num1 = int(num1)
-num2 = int(num2)
+def calc(x,y):
+    operation = input("Enter operator ( +, - , / , * ) :")
+    if operation == "+":
+        soln = x + y
+        print(f'the solution is : {soln}')
 
-if operation == "+":
-    soln = num1 + num2
-    print(f'the solution is : {soln}')
+    elif operation == "-":
+        soln = x - y
+        print(f'the solution is : {soln}')
 
-elif operation == "-":
-    soln = num1 - num2
-    print(f'the solution is : {soln}')
+    elif operation == "/":
+        soln = x / y
+        print(f'the solution is : {soln}')
 
-elif operation == "/":
-    soln = num1 / num2
-    print(f'the solution is : {soln}')
+    elif operation == "*":
+        soln = x * y
+        print(f'the solution is : {soln}')
 
-elif operation == "*":
-    soln = num1 * num2
-    print(f'the solution is : {soln}')
+    else:
+        print("enter valid operation from specified options.")
+
+try:
+    num1 = int(num1)
+    num2 = int(num2)
+    valid_input = True
+except:
+    print("error, please enter numeric values")
+    valid_input = False
+
+if valid_input == True:
+    calc(num1,num2)
+else:
+    print("could not call function due to valueerror")
+
